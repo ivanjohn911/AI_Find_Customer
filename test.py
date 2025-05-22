161b736e95136c702a0f3302178581e689ca6e17
DeepXnX <18649723134@163.com>
2025-05-22 11:15:14 +0800

    del:删除test.py文档

--- test.py
+++
@@ -1,5 +0,0 @@
-import os
-stations = ['jp','my','sg','kr','au','nz','eg','za','ae','qa','ru']
-for station in stations:
-    cmd =f'python serper_company_search.py  --general-search --custom-query "project partner of digital signature"  --gl {station} '
-    os.system(cmd)
